a126193d75e8c68ee17520463b5972e6327a27a9
Erick Ogaro <ogaroerick1998@gmail.com>
2022-09-21 13:14:07 +0300

    env variables test

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+const CURRENT_ENV = process.env.CURRENT_ENV;
+
 async function main() {
-  while(true) {
-    console.log('Travis CI update!');
+  while (true) {
+    console.log(`Microservice currently running on ${CURRENT_ENV}!`);
     await sleep(5000);
   }
 }
